20b1a13e257d06b21e04b61133f1656895147a41
rsnazario <rafael.s.nazario@gmail.com>
2020-08-25 13:24:51 -0300

    Added First Controllers of Javascript

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,25 @@
 import { ToDoItem } from './modules/toDoItem';
+
+const DomController = (function(){
+
+});
+
+const NewProjectController = (function (){
+  function newProject() {
+    
+  }
+});
+
+const Controller = (function (DOM, Task) {
+  function setupEventListeners() {
+    document.querySelector('.new-project').addEventListener('click', Task.newProject)
+  };
+
+  return {
+    init(): {
+      setupEventListeners();
+    }
+  };
+}(DomController, NewProjectController));
+
+Controller.init();
