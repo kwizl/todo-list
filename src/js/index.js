2223d2aee92e5a610680ed0b9e89437716de78d1
rsnazario <rafael.s.nazario@gmail.com>
2020-08-25 14:58:03 -0300

    setup event listener for add task button

--- src/js/index.js
+++ src/js/index.js
@@ -1,22 +1,40 @@
 import { ToDoItem } from './modules/toDoItem';
 
+const listOfProjects = [];
+
 const DomController = (function(){
 
 });
 
-const NewProjectController = (function (){
-  function newProject() {
-    
-  }
-});
+const NewProjectController = (function (itemObj){ // this one deals with form and class
+  function newTask() {
+    const addForms = document.forms['bla'];
+    console.log('started new task');
+  };
+
+
+  return { newTask, taskSubmit };
+}(ToDoItem()));
 
 const Controller = (function (DOM, Task) {
+  function newProject() {
+    // 1 - Display the form for it
+    document.querySelector('.project-form').classList.add('active');
+    // 2 - Send Information to NewProjectController (name to be redefined)
+    Task.newTask();
+
+    // 3 - Cast DOM 
+
+    // 4 - Hide Form
+  };
+  
   function setupEventListeners() {
-    document.querySelector('.new-project').addEventListener('click', Task.newProject)
+    document.querySelector('.new-project').addEventListener('click', newProject);
+    document.querySelector('#bla').addEventListener('submit', Task.taskSubmit(event));
   };
 
   return {
-    init(): {
+    init() {
       setupEventListeners();
     }
   };
